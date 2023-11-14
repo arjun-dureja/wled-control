99b7810f634026f883af9faecebae1041b79591a
arjun-dureja <arjun.dureja1000@gmail.com>
2023-11-13 22:53:19 -0500

    Initial Commit

---
+++ WLEDControl/WLEDControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  WLEDControlApp.swift
+//  WLEDControl
+//
+//  Created by Arjun Dureja on 2023-11-13.
+//
+
+import SwiftUI
+
+@main
+struct WLEDControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
